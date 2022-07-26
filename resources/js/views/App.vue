f8dc37e326a4e4493d9f2c015d8d033e8f533265
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-07-27 01:26:40 +0200

    add first page with vue

--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,14 +1,33 @@
  <template>
    <div>
       <h1>
-         hello vuejs with laravel
+        BLOG
       </h1>
+      <ul>
+        <li v-for="post in posts" :key="post.slug">{{post.title}}</li>
+      </ul>
    </div>
  </template>
  
  <script>
+import Axios from 'axios';
+
  export default {
    name: 'App',
+   data(){
+        return {
+          posts : []
+      };
+    },
+    created(){
+       Axios.get('http://127.0.0.1:8000/api/posts')
+       .then((response) =>{
+          this.posts =  (response.data);
+       })
+       .catch((e) =>{
+        console.log(e);
+       });
+    },
  }
  </script>
  
