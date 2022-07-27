bd6966f877d03067ba356ec8d054ab3dc661e275
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-07-27 14:17:04 +0200

    add all vue components

--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,33 +1,26 @@
  <template>
    <div>
-      <h1>
-        BLOG
-      </h1>
-      <ul>
-        <li v-for="post in posts" :key="post.slug">{{post.title}}</li>
-      </ul>
+      <BaseHeader/>
+      <BaseMain/>
+      <BaseFooter/>
    </div>
  </template>
  
  <script>
-import Axios from 'axios';
+
+import BaseHeader from '../components/macro/BaseHeader.vue';
+import BaseMain from '../components/macro/BaseMain.vue';
+import BaseFooter from '../components/macro/BaseFooter.vue';
 
  export default {
    name: 'App',
-   data(){
-        return {
-          posts : []
-      };
+   
+    components:{
+         BaseHeader,
+         BaseMain,
+         BaseFooter,
     },
-    created(){
-       Axios.get('http://127.0.0.1:8000/api/posts')
-       .then((response) =>{
-          this.posts =  (response.data);
-       })
-       .catch((e) =>{
-        console.log(e);
-       });
-    },
+    
  }
  </script>
  
